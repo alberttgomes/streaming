085e1cb91c4dc3a7227f800e1184771f225781c9
Albert Gomes <albert.gomes@liferay.com>
2023-09-10 15:04:30 -0300

    Create MessageConstants class

--- modules/ebook-notifications/src/main/java/com/ebook/constants/MessageConstants.java
+++ modules/ebook-notifications/src/main/java/com/ebook/constants/MessageConstants.java
@@ -1,2 +1,21 @@
-package com.ebook.constants;public class MessagensConstant {
+package com.ebook.constants;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class MessageConstants {
+    public static final String NEWS_EBOOK_MESSAGE =
+            "New news ebooks";
+
+    public static final String LAUNCHES_EBOOK_MESSAGE =
+            "Last launches ebooks";
+
+    public static final String BUY_APPROVED_MESSAGE =
+            "Was buy approved with success";
+
+    public static final String BUY_PENDING_MESSAGE =
+            "Buy is pending";
+
+    public static final String BUY_REPROVED_MESSAGE =
+            "Buy was reproved";
 }
