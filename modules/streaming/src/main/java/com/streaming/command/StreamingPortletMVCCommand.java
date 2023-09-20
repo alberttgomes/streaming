071504a13361d7f1cc6453460a62908f7687ae62
Albert Gomes <albert.gomes@liferay.com>
2023-09-20 12:00:52 -0300

    Create StreamingPortletMVCCommand class

--- modules/streaming/src/main/java/com/streaming/command/StreamingPortletMVCCommand.java
+++ modules/streaming/src/main/java/com/streaming/command/StreamingPortletMVCCommand.java
@@ -1,2 +1,31 @@
-package com.streaming.command;public class StreamingPortletMVCCommand {
+package com.streaming.command;
+
+import com.streaming.service.StreamingService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.time.Instant;
+
+@Component(
+        property = {
+                "osgi.command.function=launch",
+                "osgi.command.scope=streaming"
+        },
+        service = StreamingPortletMVCCommand.class
+)
+public class StreamingPortletMVCCommand {
+
+    @Activate
+    public void activePortletMVCommand() {
+        Instant instant = Instant.now();
+
+        System.out.println(
+                instant + "\\s" +
+                _streamingService.getPortletContent());
+    }
+
+    @Reference
+    private StreamingService _streamingService;
+
 }
