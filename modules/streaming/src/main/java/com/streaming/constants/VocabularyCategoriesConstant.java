f82084513a582f0f9997df639af3661e74e2c7ea
Albert Gomes <albert.gomes@liferay.com>
2023-10-16 09:44:01 -0300

    Create VocabularyCategoriesConstant

---
+++ modules/streaming/src/main/java/com/streaming/constants/VocabularyCategoriesConstant.java
@@ -0,0 +1,8 @@
+package com.streaming.constants;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class VocabularyCategoriesConstant {
+    public static String VOCABULARY_STREAMING_NAME = "vocabulary_streaming";
+}
