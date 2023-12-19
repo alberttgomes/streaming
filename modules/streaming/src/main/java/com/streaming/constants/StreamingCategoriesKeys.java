899926074b87866bacf400e6a2eb8d0da2a39290
Albert Gomes <albert.gomes@liferay.com>
2023-12-19 20:16:27 -0300

    Create categorization keys contants class

---
+++ modules/streaming/src/main/java/com/streaming/constants/StreamingCategoriesKeys.java
@@ -0,0 +1,10 @@
+package com.streaming.constants;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class StreamingCategoriesKeys {
+    public static String STREAMING_MOVIES_CATEGORY = "moviescategory";
+
+    public static String STREAMING_SERIES_CATEGORY = "seriescategory";
+}
