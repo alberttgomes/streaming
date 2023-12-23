191f94f48f127722314a95b025bc683853bc27a7
Albert Gomes <albert.gomes@liferay.com>
2023-12-22 23:56:48 -0300

    Follow up bugs in tasks

--- env-start.sh
+++ env-start.sh
@@ -1,6 +1,69 @@
 #!/bin/sh
 
-pattern="--"
+pattern=$1
+
+check_task()
+{
+    handle_flag "$1"
+}
+
+cleanup() 
+{
+    docker rm stg-liferay-1
+} 
+
+deploy()
+{   
+    relativePath="/dev/projects/hotel/MePortlet/streaming"
+
+    cd ~/"$relativePath"/modules || exit
+    blade gw clean deploy
+    cd ../
+    cd ~/"$relativePath"/themes || exit
+    blade gw clean deploy
+    cd ../
+
+    echo "Deploy finished."
+}
+
+docker_compose() 
+{
+    docker compose -p stg up --build
+}
+
+docker_compose_service()
+{
+   docker compose -p stg up --build "${1}"
+}
+
+handle_flag() {
+    echo "$1"
+    case "$1" in
+        "clean")
+            cleanup ;;
+        "deploy")
+            deploy ;;
+        "help")
+            help ;;
+        *)
+            echo "Unknown flag: $1"
+            exit 1 ;;
+    esac
+}
+
+help() 
+{
+    cat <<EOF
+ *** See the available commands below: *** 
+
+1. - help [Explains usage and available commands]
+2. - deploy [Deploys modules and themes before starting]
+3. - clean [Cleans the container before starting]
+
+In addion, you can call specific services by adding the service name as a parameter.
+EOF
+    exit 1
+}
 
 if [ $# -eq 0 ]
 then
@@ -8,7 +71,7 @@ then
     docker_compose
 elif [ $# -ne 0 ]
 then
-    if echo "$1" | grep -q $pattern
+    if echo "$1" | grep -q "$pattern"
     then
         check_task "$@"
     fi
@@ -17,57 +80,3 @@ else
     docker_compose_service "$@"
 fi
 
-check_task()
-{
-    if echo "$1" | grep -q "clean"
-    then
-        cleanup
-    elif echo "$1" | grep -q "deploy"
-    then 
-        deploy
-    elif echo "$1" | grep -q "help"
-    then 
-        help
-    else
-        echo "Unknown flag {$1}" 
-        exit 1
-    fi
-}
-
-cleanup() 
-{
-    echo "docker rm stg-liferay-1"
-} 
-
-deploy()
-{
-    cd /modules || exit
-    echo "blade gw clean deploy"
-    cd ../
-    cd /themes || exit
-    echo "blade gw clean deploy"
-    cd ../
-}
-
-docker_compose() 
-{
-    echo "docker compose -p stg up --build"
-}
-
-docker_compose_service()
-{
-    echo "docker compose -p stg up --build ${1}"
-}
-
-help() 
-{
-    echo " *** See the commands availables bellow: *** "
-    echo 
-    echo "1 - help [Explains usage and commands availables]"
-    echo "2 - deploy [To deploying the modules and themes before to be started]"
-    echo "3 - clean [To cleaning the container before to be started]"
-    echo
-    echo "In addition, you can use to call specific services, adding the service name as a parameter"
-
-    exit 1
-}
